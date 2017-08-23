213ba09e774a469722e12daa0fc5b15e0f3b83eb
Espana1986 <espana.romeo@gmail.com>
2017-08-23 18:39:15 -0500

    EOD Push, SPI added- visable on scope

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,6 +3,7 @@
 #include <stm32f0xx_gpio.h>
 #include <stm32f0xx_rcc.h>
 #include <stm32f0xx_i2c.h>
+#include <stm32f0xx_spi.h>
 
 //SHT31-D  declarations
 #define SHD_SDA GPIO_Pin_11
@@ -13,6 +14,9 @@
 #define SHD_SCL_PS GPIO_PinSource10
 #define SHD_PIN_AF GPIO_AF_1
 #define SHD_I2C I2C2
+
+
+
 
 //SHD-31-D I2C address - If Error Shift Address left by one due to libary fault
 #define SHDAddr (0x44<<1)
@@ -39,20 +43,12 @@ void I2C_WrReg(uint8_t Reg, uint8_t Val)
 {
 	//Wait until I2C isn't busy
 	while(I2C_GetFlagStatus(SHD_I2C, I2C_FLAG_BUSY) == SET);
-	//"Handle" a transfer - The STM32F0 series has a shocking
-	//I2C interface... Regardless! Send the address of the SHD
-	//sensor down the I2C Bus and generate a start saying we're
-	//going to write one byte. I'll be completely honest,
-	//the I2C peripheral doesn't make too much sense to me
 	//and a lot of the code is from the Std peripheral library
 	I2C_TransferHandling(SHD_I2C, SHDAddr, 1, I2C_Reload_Mode, I2C_Generate_Start_Write);
-
 	//Ensure the transmit interrupted flag is set
 	while(I2C_GetFlagStatus(SHD_I2C, I2C_FLAG_TXIS) == RESET);
-
 	//Send the address of the register we wish to write to
 	I2C_SendData(SHD_I2C, Reg);
-
 	//Ensure that the transfer complete reload flag is
 	//set, essentially a standard TC flag
 	while(I2C_GetFlagStatus(SHD_I2C, I2C_FLAG_TCR) == RESET);
@@ -163,8 +159,24 @@ uint8_t I2C_RdReg(int8_t Reg, int8_t *Data, uint8_t DCnt)
 	//Return a single piece of data if DCnt was
 	//less than 1, otherwise 0 will be returned.
 	return SingleData;
-	
 }
+
+void SPI_SendData(uint8_t adress, uint8_t data){
+	 
+	GPIO_ResetBits(GPIOB, GPIO_Pin_12);
+	 
+	while(!SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE)); 
+	SPI_SendData8(SPI1, adress);
+//	while(!SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE));
+//	SPI_ReceiveData8(SPI1);
+//	while(!SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE)); 
+//	SPI_SendData8(SPI1, data);
+//	while(!SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE));
+//	SPI_ReceiveData8(SPI1);
+	 
+	GPIO_SetBits(GPIOB, GPIO_Pin_12);
+}
+ 
 //Convert an array to an unsigned byte/word/dword
 //Type = 0 for u8
 //Type = 1 for u16
@@ -189,91 +201,75 @@ void SysTick_Handler(void)
 
 	switch (tick++)
 	{
-  	case 250:
+  	case 500:
   		tick = 0;
   		GPIOA->ODR ^= (1 << 10);
-		I2C_WrReg(0xF3,0x2D);
+//		I2C_WrReg(0xF3,0x2D);
 //		Delay(1);
+		 uint8_t DCnt = 4;	
 //		I2C_RdReg(int8_t Reg, int8_t *Data, uint8_t DCnt) DCnt in bytes
-		I2C_SHDRd(SHD_Data,4); 
+//		I2C_SHDRd(SHD_Data,DCnt);
+	
 		break;
 	}
 	
 }
 
-
-//Standard delay function as described in one
-//of my previous tutorials!
-//All it does is operate a nop instruction
-//until "Time" amount of milliseconds has passed.
-void Delay(uint32_t Time)
+void SPI1_Init(void)
 {
-	volatile uint32_t MSStart = MSec;
-	while((MSec-MSStart)<Time) asm volatile("nop");
-
+	 
+	 
+	//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+	 
+	SPI_InitTypeDef SPI_InitTypeDefStruct;
+ 	 
+	SPI_InitTypeDefStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+	SPI_InitTypeDefStruct.SPI_Mode = SPI_Mode_Master;
+	SPI_InitTypeDefStruct.SPI_DataSize = SPI_DataSize_8b;
+	SPI_InitTypeDefStruct.SPI_CPOL = SPI_CPOL_Low;
+	SPI_InitTypeDefStruct.SPI_CPHA = SPI_CPHA_1Edge;
+	SPI_InitTypeDefStruct.SPI_NSS = SPI_NSS_Soft;
+	SPI_InitTypeDefStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
+	SPI_InitTypeDefStruct.SPI_FirstBit = SPI_FirstBit_MSB;
+	 
+	SPI_Init(SPI1, &SPI_InitTypeDefStruct);
+	 
+	//	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB , ENABLE);
+	 
+	GPIO_InitTypeDef GPIO_InitTypeDefStruct;
+	 
+	GPIO_InitTypeDefStruct.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5;
+	GPIO_InitTypeDefStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitTypeDefStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitTypeDefStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitTypeDefStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOB, &GPIO_InitTypeDefStruct);
+	 
+	//Setup nSS
+	GPIO_InitTypeDefStruct.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitTypeDefStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitTypeDefStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitTypeDefStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitTypeDefStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_Init(GPIOB, &GPIO_InitTypeDefStruct);
+	//SEE TABLE 13. P.34 RM0360 for AF Table
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_0);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_0);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_0);
+	//Chip Select Pin nSS PB12 
+	GPIO_SetBits(GPIOB, GPIO_Pin_12);
+	 
+	 
+	SPI_Cmd(SPI1, ENABLE);
+	 
 }
 
-
-/*//Every x ticks, flip the GPIOA register mask to flash LED 
-void SysTick_Handler(void)
+//Start I2C2 Bus All Clocks Must Be Started Before Calling
+void I2C2_Init()
 {
-  static uint16_t tick = 0;
-
-  switch (tick++)
-  {
-  	case 50:
-  		tick = 0;
-  		GPIOA->ODR ^= (1 << 10);
-		I2C_RdReg(0,0,0);
-
-
-		break;
-  }
-}
-*/
-
-int main(void)
-{
-	SysTick_Config(SystemCoreClock/1000);
-
-	//RCC_HSICmd(ENABLE);
-	//while(!RCC_GetFlagStatus(RCC_FLAG_HSIRDY));
-	//RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI);
-	// Set HSION bit 
-
-	//RCC->CR |= (uint32_t)0x00000001;
-	//Enable GPIOB clock, required for the I2C output
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
-	//RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 
-	Delay(10);	
-	//Enable the I2C peripheral clock
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
-
-	/* Reset I2Cx IP */
-	RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C2, ENABLE);
-	
-	Delay(10);
-	/* Release reset signal of I2Cx IP */
-	RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C2, DISABLE);
-
-	//Enable GPIOA clock, required for LED 
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 
-
-	//Set mode Register for GPIOA
-	GPIOA->MODER = (1 << 20); //GPIO set mode register 
-	
-	//GPIOB_AFRL Set B Pin 0-7
-//	GPIOB->AFR[0] = (0x0);
-	//GPIOB_AFRH Set B Pin 8-15
-//	GPIOB->AFR[1] = (0x1100); 
-
-	//SET GPIOB MODE MASK AFTER SETTING AF REGTISTERS TO AVOID UNWANTED PIN TRANSITIONS
-//	GPIOB->MODER = (0x42A50A80);
-
 	//Configure the pins to the I2C AF
 	GPIO_PinAFConfig(SHD_GPIO, SHD_SDA_PS, SHD_PIN_AF);
 	GPIO_PinAFConfig(SHD_GPIO, SHD_SCL_PS, SHD_PIN_AF);
-
 
 	//Set the pins SHD_SDA and SHD_SCL as alternate function GPIO pins
 	GP.GPIO_Pin = SHD_SDA | SHD_SCL;
@@ -300,6 +296,58 @@ int main(void)
 	I2C_Init(SHD_I2C, &IT);
 	I2C_Cmd(I2C2, ENABLE);
 
+}
+
+//Delay function: All it does is operate a nop instruction
+//until "Time" amount of milliseconds has passed.
+void Delay(uint32_t Time)
+{
+	volatile uint32_t MSStart = MSec;
+	while((MSec-MSStart)<Time) asm volatile("nop");
+
+}
+
+
+int main(void)
+{
+	SysTick_Config(SystemCoreClock/1000);
+
+	//Alternate Mask 
+	//RCC->CR |= (uint32_t)0x00000001;
+	//Enable GPIOB clock, required for the I2C output
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+	//RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 
+	Delay(10);	
+	//Enable the I2C peripheral clock
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
+
+	/* Reset I2Cx IP */
+	RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C2, ENABLE);
+
+	Delay(10);
+	/* Release reset signal of I2Cx IP */
+	RCC_APB1PeriphResetCmd(RCC_APB1Periph_I2C2, DISABLE);
+
+	//Enable GPIOA clock, required for LED 
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 
+
+	//SPI 
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+
+	//Set mode Register for GPIOA
+	GPIOA->MODER = (1 << 20); //GPIO set mode register 
+
+	I2C2_Init();
+ 	SPI1_Init();
+	//GPIOB_AFRL Set B Pin 0-7
+//	GPIOB->AFR[0] = (0x0);
+	//GPIOB_AFRH Set B Pin 8-15
+//	GPIOB->AFR[1] = (0x1100); 
+
+	//SET GPIOB MODE MASK AFTER SETTING AF REGTISTERS TO AVOID UNWANTED PIN TRANSITIONS
+//	GPIOB->MODER = (0x42A50A80);
+
 //	I2C_RdReg(0,0,0);
 
 	//PSUDO CODE
@@ -312,18 +360,7 @@ int main(void)
 	while(1)
 	{
 
-//	I2C_RdReg(0xF32D, 0x0, 16);
-//	while(I2C_GetFlagStatus(I2C2, I2C_ISR_BUSY)==RESET);
-//	I2C_SlaveAddressConfig(I2C2, SHDAddr);
-//	I2C_GenerateSTART(I2C2, ENABLE);
-//	I2C_NumberOfBytesConfig(I2C2, 8);
-//	I2C_SendData(I2C2, 0xF3);
-//	I2C_SendData(I2C2,0b00101101);
-//	Delay(1);
-//	while(I2C_GetFlagStatus(I2C2, I2C_ISR_TC) == RESET);
-//	I2C_SendData(I2C2,0b11110011);
-//	while(I2C_GetFlagStatus(I2C2, I2C_ISR_TC) == RESET);
-//	I2C_GenerateSTOP(I2C2, ENABLE);
+	SPI_SendData(0x04,0x00);
 	Delay(500);	
 	}	
 }
